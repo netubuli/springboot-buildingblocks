f8753735220b5faa806b77006dd851e1c395e1db
netubuli <netubuli@gmail.com>
2020-06-06 13:24:29 +0300

    internationalization first commit

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.stacksimplify.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	ResourceBundleMessageSource messageSource;
 	
 	//@RequestMapping(method=RequestMethod.GET, path="/helloworld")
 	@GetMapping("/helloworld")
@@ -18,5 +28,16 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Nelson", "Etubuli", "Kisumu");
 	}
-
+	
+	@GetMapping("/hello_int")
+	public String getMessagesinI18n(@RequestHeader(name="Accept-Language",required = false) String locale ) {
+		return messageSource.getMessage("label.hello", null,new Locale(locale));
+		
+	}
+	
+	@GetMapping("/hello_int2")
+	public String getMessagesinI18n()  {
+		return messageSource.getMessage("label.hello", null,LocaleContextHolder.getLocale() );
+		
+	}
 }
--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -1,13 +1,18 @@
 package com.stacksimplify.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 
 @SpringBootApplication
@@ -19,6 +24,19 @@ public class SpringbootBuildingblocksApplication {
 //demo git staging
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+	@Bean
+	public AcceptHeaderLocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+		
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
